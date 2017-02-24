61c4ae52f0bc004fac81fb58a74aafdf46c3189f
Ben Campbell <benjaminjcampbell@fs.fed.us>
2017-02-24 16:20:46 -0700

    change data_files paths

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 import py2exe
 from glob import glob
 
-data_files = [("Microsoft.VC90.CRT", glob(r'C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*')), 'Python.Runtime.dll', 'System.Data.SQLite.dll', 'CruiseDAL.dll', 'RunCOConvert.bat']
+data_files = [(".\Microsoft.VC90.CRT", glob(r'C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*')), '.\Python.Runtime.dll', '.\System.Data.SQLite.dll', '.\CruiseDAL.dll', '.\RunCOConvert.bat']
 
 setup(
     data_files=data_files,
